933a816e57685c2871c1ca8014b9c976fc24edda
유혜림 <sibiwol12@gmail.com>
2021-05-15 15:18:32 +0900

    js: navbar menu click

--- portfolio/lecture/main.js
+++ portfolio/lecture/main.js
@@ -3,6 +3,12 @@
 const navbar = document.querySelector("#navbar");
 const home = document.querySelector("#home");
 const about = document.querySelector("#about");
+const skills = document.querySelector("#skills");
+const work = document.querySelector("#work");
+const testimonials = document.querySelector("#testimonials");
+const contact = document.querySelector("#contact");
+
+const contactBtn = document.querySelector(".home__contact");
 
 // navbar가 top에 올라가면 background-color = transparent
 window.addEventListener("scroll", () => {
@@ -13,4 +19,17 @@ window.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("color--dark");
   }
+});
+
+// handle scolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const dataLink = target.dataset.link;
+  const scrollTo = document.querySelector(dataLink);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
+
+contactBtn.addEventListener("click", () => {
+  contact.scrollIntoView({ behavior: "smooth" });
 });
