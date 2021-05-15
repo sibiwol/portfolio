566934784d67cbc3af3a0e064d12a8f7c9755eb3
유혜림 <sibiwol12@gmail.com>
2021-05-15 15:25:23 +0900

    js: scrollIntoView(selctor)함수 만듬

--- portfolio/lecture/main.js
+++ portfolio/lecture/main.js
@@ -8,8 +8,6 @@ const work = document.querySelector("#work");
 const testimonials = document.querySelector("#testimonials");
 const contact = document.querySelector("#contact");
 
-const contactBtn = document.querySelector(".home__contact");
-
 // navbar가 top에 올라가면 background-color = transparent
 window.addEventListener("scroll", () => {
   const navbarHeight = navbar.getBoundingClientRect().height;
@@ -26,10 +24,16 @@ const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (e) => {
   const target = e.target;
   const dataLink = target.dataset.link;
-  const scrollTo = document.querySelector(dataLink);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(dataLink);
 });
 
+//  handle click on "contact me" button on home
+const contactBtn = document.querySelector(".home__contact");
 contactBtn.addEventListener("click", () => {
-  contact.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView("#contact");
 });
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
